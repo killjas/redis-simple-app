c1e751c7f31fb99c3e68718aafba98b1ad7f675c
Dmitry Barskov <dmitry.barskov64@gmail.com>
2020-07-15 22:09:33 +0300

    Count visits, use env variable for redis url

--- src/main/java/ru/itis/redis/SimpleController.java
+++ src/main/java/ru/itis/redis/SimpleController.java
@@ -4,27 +4,32 @@ import org.redisson.Redisson;
 import org.redisson.api.RBucket;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
-    private static int Counter = 0;
+    private final RedissonClient redisClient;
+
+    public SimpleController(RedissonClient redisClient) {
+        this.redisClient = redisClient;
+    }
 
     @GetMapping("/redis")
     public ResponseEntity<?> count() {
-        RedissonClient redissonClient = getRedisson();
-        RBucket<String> bucket = redissonClient.getBucket("simpleObject");
-        bucket.set(String.valueOf(Counter++));
-        redissonClient.shutdown();
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(visits());
     }
 
-    private RedissonClient getRedisson() {
-        Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
-        RedissonClient redisson = Redisson.create(config);
-        return redisson;
+    private int visits() {
+        RBucket<String> bucket = redisClient.getBucket("VISITS");
+        String value = bucket.get();
+        if (value == null) {
+            value = "0";
+        }
+        int visits = Integer.parseInt(value);
+        bucket.set(String.valueOf(visits + 1));
+        return visits;
     }
 }
--- src/main/java/ru/itis/redis/config/RedisConfig.java
+++ src/main/java/ru/itis/redis/config/RedisConfig.java
@@ -1,8 +1,25 @@
 package ru.itis.redis.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
+    @Bean
+    @Qualifier("redisUrl")
+    public String redisPort() {
+        return System.getenv("REDIS_URL");
+    }
 
+    @Bean
+    public RedissonClient redisClient(@Autowired @Qualifier("redisUrl") String redisUrl) {
+        Config config = new Config();
+        config.useSingleServer().setAddress(redisUrl);
+        return Redisson.create(config);
+    }
 }

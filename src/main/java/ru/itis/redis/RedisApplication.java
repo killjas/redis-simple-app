f6eff01e5f81e91165bc0d32b00abbff84015547
Aina <ainaard@mail.ru>
2020-07-16 01:16:30 +0300

    created dockerfile and docker-compose

--- src/main/java/ru/itis/redis/RedisApplication.java
+++ src/main/java/ru/itis/redis/RedisApplication.java
@@ -2,10 +2,13 @@ package ru.itis.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import redis.clients.jedis.Jedis;
 
 @SpringBootApplication
 public class RedisApplication {
     public static void main(String[] args) {
+        Jedis jedis = new Jedis("redis");
+        System.out.println("Server is running: " + jedis.ping());
         SpringApplication.run(RedisApplication.class, args);
     }
 }
